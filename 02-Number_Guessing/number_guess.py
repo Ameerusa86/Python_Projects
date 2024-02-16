80912e9e4cde23f52e885ccfaff7f8a8b6e3517b
Ameer <46317379+Ameerusa86@users.noreply.github.com>
2024-02-15 21:15:22 -0500

    Guessing Game

---
+++ 02-Number_Guessing/number_guess.py
@@ -0,0 +1,31 @@
+# Guessing Game Number 1 - 10
+
+import random
+
+
+def get_number() -> int:
+    return random.randint(1, 10)
+
+
+def get_guess() -> int:
+    return int(input("Enter a number between 1 and 10: "))
+
+
+def guess_game():
+    number = get_number()
+    guess = get_guess()
+    while guess != number:
+        if guess < number:
+            print("Too low!")
+            guess = get_guess()
+        elif guess > number:
+            print("Too high!")
+            guess = get_guess()
+        if guess == number:
+            print("You got it!")
+            guess = get_guess()
+            break
+
+
+if __name__ == "__main__":
+    guess_game()
